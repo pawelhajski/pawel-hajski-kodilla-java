fb64629c6deec3e2fecc9df969da696013796362
Mondi <p.hajski@gmail.com>
2019-04-13 12:28:39 +0200

    SimpleUser.java Calculator.java created, tested

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        System.out.println("SimpleUser - test");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Calculator - test");
+
+        Calculator calculator = new Calculator();
+        int resultAdd = calculator.add(5,10);
+        int resultSubtract = calculator.subtract(15, 5);
+
+        if(resultAdd == 15) {
+            System.out.println("add test OK");
+        } else {
+            System.out.println("add test error");
+        }
+
+        if(resultSubtract == 10) {
+            System.out.println("subtract test OK");
+        } else {
+            System.out.println("subtract test error");
+        }
     }
 }
