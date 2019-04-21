4e181f3c0ee91e9f23e802d02d63511820069fe7
Mondi <p.hajski@gmail.com>
2019-04-21 16:54:12 +0200

    .immutable.Book, .ForumUser, .iterate.NumbersGenerator

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -27,5 +28,8 @@ public class StreamMain {
         poemBeautifier.beautify("This is a sample text", (textSample -> textSample.toUpperCase()));
         poemBeautifier.beautify("This is a sample text", (textSample -> textSample.toLowerCase()));
         poemBeautifier.beautify("This is a sample text", (textSample -> "++++" + textSample + "++++"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
