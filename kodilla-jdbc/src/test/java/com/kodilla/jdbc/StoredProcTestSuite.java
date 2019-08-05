6e83f9df563eef0094cbd11cd3ea1a27094550a6
Mondi <p.hajski@gmail.com>
2019-08-05 16:07:44 +0200

    Submodule 22.3

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,57 @@
+package com.kodilla.jdbc;
+
+import org.junit.Test;
+
+import javax.swing.plaf.nimbus.State;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+
+public class StoredProcTestSuite {
+
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"NOT SET\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"NOT SET\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(0, howMany);
+    }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=FALSE";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTableTrue = "SELECT COUNT(*) AS HOW_MANY_TRUE FROM BOOKS WHERE BESTSELLER=TRUE";
+        ResultSet rsTrue = statement.executeQuery(sqlCheckTableTrue);
+        int howManyTrue = -1;
+        if (rsTrue.next()) {
+            howManyTrue = rsTrue.getInt("HOW_MANY_TRUE");
+        }
+        assertEquals(1, howManyTrue);
+    }
+}
